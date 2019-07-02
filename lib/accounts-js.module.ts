4bd3983943a6bf726797a4e5e651dd6395ae2743
NickBolles <me@nickbolles.com>
2019-07-02 22:15:24 +0000

    chore: refactor buildProviders to it's own file

--- lib/accounts-js.module.ts
+++ lib/accounts-js.module.ts
@@ -1,6 +1,6 @@
 import accountsExpress from '@accounts/rest-express';
 import AccountsServer from '@accounts/server';
-import { Inject, Module, Provider } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { MODULE_PATH } from '@nestjs/common/constants';
 import {
   DynamicModule,
@@ -9,42 +9,35 @@ import {
 } from '@nestjs/common/interfaces';
 import { resolve } from 'url';
 import { debuglog } from 'util';
-import { AccountsSessionInterceptorProvider } from './interceptors/ResumeSession.interceptor';
 import {
-  AccountsModuleOptions as AccountsModuleMetadata,
-  AccountsOptions,
+  AccountsModuleOptions,
   NestAccountsOptions,
-  NestAccountsOptionsProvider,
 } from './interfaces/AccountsNestModuleOptions';
-import { AccountsServerProvider } from './providers/AccountsServer.provider';
-import { GraphQLModuleProvider } from './providers/GraphQLModule';
 import {
   ACCOUNTS_JS_GRAPHQL,
   ACCOUNTS_JS_OPTIONS,
   ACCOUNTS_JS_SERVER,
 } from './utils/accounts.constants';
+import { buildProviders } from './utils/buildProviders';
 import { getRESTOptions } from './utils/getRestOptions';
-import { isProvider } from './utils/typeguards';
-import { accountsOptionsToProvider } from './providers/AccountsOptions.provider';
 const debug = debuglog('nestjs-accounts');
 
-@Module({
-  exports: [ACCOUNTS_JS_SERVER, ACCOUNTS_JS_OPTIONS, ACCOUNTS_JS_GRAPHQL],
-})
+@Module({})
 export class AccountsJsModule implements NestModule {
   /**
    * Register and configure the AccountsJsModule.
    *
-   * @param {AccountsModuleMetadata} metadata for the accounts module
+   * @param {AccountsModuleOptions} metadata for the accounts module
    * @returns {DynamicModule} Nest module
    */
-  static register(metadata: AccountsModuleMetadata): DynamicModule {
-    const { accountsOptions, useServer, ...accountsModuleMetadata } = metadata;
+  static register(metadata: AccountsModuleOptions): DynamicModule {
+    const { accountsOptions, useServer, ...AccountsModuleOptions } = metadata;
 
     return {
       module: AccountsJsModule,
-      ...accountsModuleMetadata,
+      ...AccountsModuleOptions,
       providers: buildProviders(metadata),
+      exports: [ACCOUNTS_JS_SERVER, ACCOUNTS_JS_OPTIONS, ACCOUNTS_JS_GRAPHQL],
     };
   }
 
@@ -59,70 +52,23 @@ export class AccountsJsModule implements NestModule {
    */
   configure(consumer: MiddlewareConsumer) {
     if (this.options.REST) {
-      configureREST(consumer, this.options, this.accountsServer);
+      const { path, relative = true, ...opts } = getRESTOptions(this.options);
+      const nestPath = Reflect.getMetadata(MODULE_PATH, AccountsJsModule);
+
+      let pathToUse: string;
+      if (relative && nestPath) {
+        pathToUse = resolve(nestPath, path || '');
+      } else {
+        // absolute path
+        pathToUse = path || '/accounts';
+      }
+
+      debug(`mounting @accounts/rest-express on path '${pathToUse}'`);
+      consumer
+        // forRoutes will scope this middleware to it's route and trim the prefix, we'll
+        // mount the accountsExpress middleware without a path and use forRoutes to define the prefix
+        .apply(accountsExpress(this.accountsServer, { path: '', ...opts }))
+        .forRoutes(pathToUse);
     }
   }
 }
-
-function configureREST(
-  consumer: MiddlewareConsumer,
-  options: NestAccountsOptions,
-  accountsServer: AccountsServer,
-) {
-  const { path, relative = true, ...opts } = getRESTOptions(options);
-  const nestPath = Reflect.getMetadata(MODULE_PATH, AccountsJsModule);
-
-  let pathToUse: string;
-  if (relative && nestPath) {
-    pathToUse = resolve(nestPath, path || '');
-  } else {
-    // absolute path
-    pathToUse = path || '/accounts';
-  }
-
-  debug(`mounting @accounts/rest-express on path '${pathToUse}'`);
-  consumer
-    // forRoutes will scope this middleware to it's route and trim the prefix, we'll
-    // mount the accountsExpress middleware without a path and use forRoutes to define the prefix
-    .apply(accountsExpress(accountsServer, { path: '', ...opts }))
-    .forRoutes(pathToUse);
-}
-
-/**
- * Check and get the providers from the options passed in.
- * This will create the custom providers for the ACCOUNTS_JS_SERVER and ACCOUNTS_JS_OPTIONS
- * so that they can be injected anywhere with Nest.
- *
- * This will also create the AccountsSessionInterceptor as a global interceptor
- * so that the accounts session is restored on every request.
- * s
- * @param {AccountsModuleMetadata} options for the accounts module
- */
-function buildProviders(options: AccountsModuleMetadata): Provider[] {
-  const { useServer, accountsOptions, providers = [] } = options;
-
-  if (useServer && accountsOptions) {
-    throw new Error(
-      'accountsOptions will be ignored when passing an existing server instance',
-    );
-  }
-
-  let serverProvider: Provider = AccountsServerProvider;
-  // If an existing server instance is passed in, use that instead of the factory
-  if (useServer) {
-    serverProvider = {
-      provide: ACCOUNTS_JS_SERVER,
-      useValue: useServer,
-    };
-  }
-
-  return [
-    ...providers,
-    AccountsSessionInterceptorProvider,
-    accountsOptionsToProvider(accountsOptions),
-    serverProvider,
-    GraphQLModuleProvider,
-  ];
-}
-
-// todo: auth guard
---
+++ lib/utils/buildProviders.ts
@@ -0,0 +1,44 @@
+import { Provider } from '@nestjs/common';
+import { AccountsSessionInterceptorProvider } from '../interceptors/ResumeSession.interceptor';
+import { AccountsModuleOptions } from '../interfaces/AccountsNestModuleOptions';
+import { accountsOptionsToProvider } from '../providers/AccountsOptions.provider';
+import { AccountsServerProvider } from '../providers/AccountsServer.provider';
+import { ACCOUNTS_JS_SERVER } from './accounts.constants';
+import { GraphQLModuleProvider } from '../providers/GraphQLModule';
+
+/**
+ * Check and get the providers from the options passed in.
+ * This will create the custom providers for the ACCOUNTS_JS_SERVER and ACCOUNTS_JS_OPTIONS
+ * so that they can be injected anywhere with Nest.
+ *
+ * This will also create the AccountsSessionInterceptor as a global interceptor
+ * so that the accounts session is restored on every request.
+ * s
+ * @param {AccountsModuleOptions} options for the accounts module
+ */
+export function buildProviders(options: AccountsModuleOptions): Provider[] {
+  const { useServer, accountsOptions, providers = [] } = options;
+
+  if (useServer && accountsOptions) {
+    throw new Error(
+      'accountsOptions will be ignored when passing an existing server instance',
+    );
+  }
+
+  let serverProvider: Provider = AccountsServerProvider;
+  // If an existing server instance is passed in, use that instead of the factory
+  if (useServer) {
+    serverProvider = {
+      provide: ACCOUNTS_JS_SERVER,
+      useValue: useServer,
+    };
+  }
+
+  return [
+    ...providers,
+    AccountsSessionInterceptorProvider,
+    accountsOptionsToProvider(accountsOptions),
+    serverProvider,
+    GraphQLModuleProvider,
+  ];
+}
