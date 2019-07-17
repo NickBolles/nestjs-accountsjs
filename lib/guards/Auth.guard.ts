1a37379b08a558c87dd430b362f9ad0435b89fbf
Nick Bolles <me@nickbolles.com>
2019-07-17 00:37:29 +0000

    fix(guards): export AuthGuard

--- lib/guards/Auth.guard.ts
+++ lib/guards/Auth.guard.ts
@@ -1,15 +1,13 @@
 import AccountsServer from '@accounts/server';
 import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
 import { Observable } from 'rxjs';
-import { AccountsSessionRequest } from '../interfaces/AccountsRequest';
 import { ACCOUNTS_JS_SERVER } from '../utils/accounts.constants';
 import { GetFieldFromExecContext } from '../utils/GraphQLUtils';
 
 // todo: allow customization of this
 // todo: unify context better. It sounds like this will break see issue: https://github.com/nestjs/nest/issues/1581 and PR https://github.com/nestjs/nest/pull/2493
 @Injectable()
-export class Auth implements CanActivate {
+export class AuthGuard implements CanActivate {
   constructor(
     @Inject(ACCOUNTS_JS_SERVER)
     private readonly accountsServer: AccountsServer,
--- lib/guards/index.ts
+++ lib/guards/index.ts
@@ -1,1 +1,2 @@
+export * from './Auth.guard';
 export * from './EnableWithService.guard';
