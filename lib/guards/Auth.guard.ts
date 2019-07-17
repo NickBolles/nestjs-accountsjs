71bb2f899c1f0fe727961028e3e8a45a00163872
Nick Bolles <me@nickbolles.com>
2019-07-17 00:41:28 +0000

    fix(guards): cleanup AuthGuard

--- lib/guards/Auth.guard.ts
+++ lib/guards/Auth.guard.ts
@@ -1,18 +1,11 @@
-import AccountsServer from '@accounts/server';
-import { CanActivate, ExecutionContext, Inject, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { ACCOUNTS_JS_SERVER } from '../utils/accounts.constants';
 import { GetFieldFromExecContext } from '../utils/GraphQLUtils';
 
 // todo: allow customization of this
 // todo: unify context better. It sounds like this will break see issue: https://github.com/nestjs/nest/issues/1581 and PR https://github.com/nestjs/nest/pull/2493
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(
-    @Inject(ACCOUNTS_JS_SERVER)
-    private readonly accountsServer: AccountsServer,
-  ) {}
-
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     return !!GetFieldFromExecContext(context);
   }
