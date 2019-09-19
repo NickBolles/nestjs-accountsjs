7a1d79dfae77effdc43a1d391f11b768ebaf5908
Nick Bolles <me@nickbolles.com>
2019-09-19 01:47:27 +0000

    test: POC for tests for examples

--- examples/basic-value-opts/app.module.ts
+++ examples/basic-value-opts/app.module.ts
@@ -8,24 +8,22 @@ import { UserDatabase } from '../shared/database.service';
 @Module({
   imports: [
     ConfigModule.load(resolve(__dirname, 'config', '**/!(*.d).{ts,js}')),
-    AccountsJsModule.register({
+    AccountsJsModule.registerAsync({
       providers: [UserDatabase],
-      accountsOptions: {
-        serverOptions: {
-          db: new UserDatabase(),
-          tokenSecret: 'secret',
-        },
-        services: {
-          password: new AccountsPassword(),
-        },
-        // With no options you can just use true
-        // REST: true,
-        REST: {
-          path: '/',
-          transformOAuthResponse: loginResult => loginResult,
-          onOAuthError: (req, res, err) => console.log('Oauth Error'),
-          onOAuthSuccess: (req, res, err) => console.log('Oauth Success'),
-        },
+      serverOptions: {
+        db: new UserDatabase(),
+        tokenSecret: 'secret',
+      },
+      services: {
+        password: new AccountsPassword(),
+      },
+      // With no options you can just use true
+      // REST: true,
+      REST: {
+        path: '/',
+        transformOAuthResponse: loginResult => loginResult,
+        onOAuthError: (req, res, err) => console.log('Oauth Error'),
+        onOAuthSuccess: (req, res, err) => console.log('Oauth Success'),
       },
       /**
        * tip: The above is a shortcut for the custom provider and is equal to the following.
